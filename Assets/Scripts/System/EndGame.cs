bd386d3336bf63a03b99e4b5933e9e245dcdf914
shaman1979 <upgamestudio@gmail.com>
2019-03-30 20:06:57 +0300

    Добавил конец игры, вынес его в отдельный скрипт

---
+++ Assets/Scripts/System/EndGame.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+public class EndGame : MonoBehaviour, ISubscriber<EndGameValue>
+{
+    public UnityEvent OnEndGame;
+
+    public void Update(EndGameValue massage)
+    {
+        OnEndGame.Invoke();
+    }
+}
+
+public class EndGameValue
+{
+    public float Score { get; set; }
+}
