62665b14a13dcd35c7d52a20d19efa55dd264268
shaman1979 <upgamestudio@gmail.com>
2019-04-04 21:06:16 +0300

    Исправил баг с бесконечностью

---
+++ Assets/LineRendererMove.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineRendererMove : MonoBehaviour
+{
+    private float speed = 5f;
+    List<Vector3> dotPositions = new List<Vector3>();
+    [SerializeField]
+    private GameObject line;
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+
+    }
+
+    public void AddPoint()
+    {
+        
+    }
+
+}
--- Assets/Scripts/Player/Objects/Move.cs
+++ Assets/Scripts/Player/Objects/Move.cs
@@ -8,22 +8,11 @@ public class Move : MonoBehaviour
     #region Configs
     [SerializeField]
     private float speed;
-    [SerializeField]
-    private float deleteZone = -5f;
     #endregion
 
     private void Update()
     {
         Movening();
-        Destroy();
-    }
-
-    private void Destroy()
-    {
-        if (transform.position.y <= deleteZone)
-        {
-            Destroy(gameObject);
-        }
     }
 
     private void Movening()
